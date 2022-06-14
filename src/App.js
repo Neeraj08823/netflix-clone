2e8f5d20b8b93fe396b4c49e76d3f7651b6e4aef
Neeraj08823 <nk08823@gmail.com>
2022-06-14 13:38:23 +0530

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hey</h1>
+      <Row
+        title="NETFLIX ORIGNINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchTrending} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchTrending} />
+      <Row title="Horror Movie" fetchUrl={requests.fetchTrending} />
+      <Row title="Romance Movie" fetchUrl={requests.fetchTrending} />
+      <Row title="Documantaries" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
